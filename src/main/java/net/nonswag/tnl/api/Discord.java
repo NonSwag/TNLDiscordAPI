e84b4932db15777b31faa16762afa86611a06282
NonSwag <MrMinecraft00@gmail.com>
2021-03-28 16:35:35 +0200

    Update

--- src/main/java/net/nonswag/tnl/api/Discord.java
+++ src/main/java/net/nonswag/tnl/api/Discord.java
@@ -1,14 +1,12 @@
 package net.nonswag.tnl.api;
 
 import com.velocitypowered.api.plugin.Plugin;
-import com.velocitypowered.api.plugin.PluginContainer;
 import com.velocitypowered.api.proxy.ProxyServer;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.slf4j.Logger;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
-import java.util.Optional;
 
 public class Discord {
 
@@ -16,7 +14,6 @@ public class Discord {
 
         @Override
         public void onEnable() {
-            org.bukkit.Bukkit.getConsoleSender().sendMessage("§8[§f§lAPI§8] §aInitialized §6JDA v" + this.getDescription().getVersion());
         }
     }
 
@@ -25,14 +22,12 @@ public class Discord {
             id = "tnldiscordapi",
             name = "TNLDiscordAPI",
             url = "https://www.thenextlvl.net/",
-            version = "4.2.0_277"
+            version = "4.2.0_227"
     )
     public static class Velocity {
 
         @Inject
         public Velocity(@Nonnull ProxyServer server, @Nonnull Logger logger) {
-            Optional<PluginContainer> plugin = server.getPluginManager().getPlugin("tnldiscordapi");
-            plugin.ifPresent(pluginContainer -> logger.info("§8[§f§lAPI§8] §aInitialized §6JDA v" + pluginContainer.getDescription().getVersion()));
         }
     }
 }
